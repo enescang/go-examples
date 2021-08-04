d33472e2c54ba6fcaedaed1091a86ba647fad119
kodcanlisi <developerindiee@gmail.com>
2021-08-04 22:37:45 +0300

    1️⃣ sync.Once

---
+++ sync/Once/main.go
@@ -0,0 +1,32 @@
+//Enes Can Güneş - kodlib.com - 2021
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+var singleton sync.Once
+var dbInstance string
+
+func main() {
+	//Let's call DatabaseConnection function multiple times to see differences
+	for i := 0; i < 10; i++ {
+		DatabaseConnection()
+	}
+	//sync.Once runs exactly one time
+	//and if you need to create singleton pattern in your application
+	//sync.Once is better option then traditional way.
+	//Please check: https://pkg.go.dev/sync#Once
+}
+
+func DatabaseConnection() string {
+	singleton.Do(func() {
+		fmt.Println("Db Connection Started")
+		dbInstance = "connect_your_database"
+		fmt.Println("Db Connected")
+		panic("asda")
+	})
+	fmt.Println("Other Stuff")
+	return dbInstance
+}
